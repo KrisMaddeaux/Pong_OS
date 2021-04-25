841f28d268a44dbd45aaca78c71e41f609a946a9
Kristopher Maddeaux <kristophermaddeaux@gmail.com>
2021-04-24 23:31:46 -0300

    Added PrintHex functionality for debugging purposes

--- Pong_OS/boot_sect.asm
+++ Pong_OS/boot_sect.asm
@@ -7,10 +7,71 @@
 mov bx, helloWorldString
 call PrintString
 
+mov dx, 0xfd6d
+call PrintHex
+
 jmp $			; Jump to the  current  address (i.e.  forever).
 
 helloWorldString:
 	db "Hello World!", 0
+
+;------------------------------------------------------------------------
+; Print Hex
+;------------------------------------------------------------------------
+
+; Expect hex value to be in the dx register
+PrintHex:
+	mov ah, 0x0e	; int  10/ah = 0eh -> scrolling teletype BIOS routine
+
+	; Print leading "0x" hex characters
+	mov al, "0"
+	int 0x10
+	mov al, "x"
+	int 0x10
+
+	; The dx register is 16 bits (2 bytes). 1 hex byte (8 bits) is represented by 2 characters.
+	; So each individual hex character is 4 bits. dx is divided into dh and dl (8 bits each).
+	; So we can get the needed four bits by bitshifting dh and dl
+	mov al, dh
+	shr al, 4
+	call HandleHexValue
+
+	shl dh, 4
+	mov al, dh
+	shr al, 4
+	call HandleHexValue
+
+	mov al, dl
+	shr al, 4
+	call HandleHexValue
+
+	shl dl, 4
+	mov al, dl
+	shr al, 4
+	call HandleHexValue
+
+	ret
+
+HandleHexValue:
+	cmp al, 9
+		jbe HandleHexNumber
+		ja HandleHexLetter
+
+	; Convert our hex value into ascii. Conversion is just an addition (refer to ascii table)
+	HandleHexNumber:
+		add al, 0x30
+		jmp PrintHexValue
+	HandleHexLetter:
+		add al, 0x37
+		jmp PrintHexValue
+
+	PrintHexValue:
+		int 0x10		; print from al register
+	ret
+
+;------------------------------------------------------------------------
+; Print String
+;------------------------------------------------------------------------
 
 ; Expect string to be in the bx register
 PrintString:
